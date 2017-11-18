3b08b7cb12ed7aedef717e39a8032282155455ea
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2017-11-19 00:11:33 +0200

    tehtävä 10

--- WebLogin/src/main/java/ohtu/Tester.java
+++ WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,7 @@
 package ohtu;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,12 +10,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tester {
 
     public static void main(String[] args) {
+
+        ChromeDriverManager.getInstance().setup();
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
-        
+
         sleep(2);
-        
+
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
@@ -22,20 +26,60 @@ public class Tester {
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
+        element.sendKeys("wrong");
         element = driver.findElement(By.name("login"));
-        
-        sleep(2);
         element.submit();
 
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("ukkonooa");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("wrong");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+
+        Random random = new Random();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("ukko" + random.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("nooa");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("nooa");
+        element = driver.findElement(By.name("signup"));
+        element.submit();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
         sleep(3);
-        
+
         driver.quit();
     }
-    
-    private static void sleep(int n){
-        try{
-            Thread.sleep(n*1000);
-        } catch(Exception e){}
+
+    private static void sleep(int n) {
+        try {
+            Thread.sleep(n * 1000);
+        } catch (Exception e) {
+        }
     }
 }
